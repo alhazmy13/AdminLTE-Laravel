d977fc42cff0c62b9beecfa10541166447b9f343
Abdullah M. Alhazmy <aalhazmy@sasmc.net>
2016-12-14 20:18:18 +0300

    - First Commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index()
+    {
+        $task = Task::all();
+        return view('task', ['tasks' => $task]);
+    }
+}
---
+++ app/Task.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+class Task extends Model
+{
+    protected  $table ='tasks';
+
+
+}
